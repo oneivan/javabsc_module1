d0f054296b356fc18db6bccb5e10e49ea7556a08
oneivan <enekbenek@gmail.com>
2024-02-25 23:03:47 +0300

    add greetings method

---
+++ src/main/java/hw1/o2s_javabsc/MainApp.java
@@ -0,0 +1,17 @@
+package hw1.o2s_javabsc;
+
+public class MainApp {
+/*
+(1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java;
+*/
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+
+    public static void main(String[] args) {
+        greetings();
+    }
+}
